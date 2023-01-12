3d86babf4ed1c012e67697933555dd4638b28069
szoker <alex@gmail.com>
2023-01-12 14:42:55 +0100

    sessionCreate

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,9 +2,9 @@ import {
   Controller,
   Get,
   Render,
+  Session,
 } from '@nestjs/common';
 import { AppService } from './app.service';
-import db from './db';
 
 @Controller()
 export class AppController {
@@ -12,7 +12,10 @@ export class AppController {
 
   @Get()
   @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  index(@Session() session: Record<string, any>) {
+    if(!session.dobas){
+      session.dobas = Math.floor(Math.random()* 100);
+    }
+    return { message: 'A szám: ' + session.dobas };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  /*
-     Here it's assumed that public and views are in the root directory,
-     alongside with src. You can put them wherever you want, 
-     just use the correct path if you use another folder.
-  */
+  
+  app.use(
+    session({
+      secret: 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
